e01b603b625665b72d3cdc40f084417cf5e2971e
JC <jc@jc.com>
2019-07-31 21:56:03 +0200

    Use custom component for operations history

--- src/action/ProcessAction.java
+++ src/action/ProcessAction.java
@@ -1,23 +1,16 @@
 package action;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTextPane;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.SimpleAttributeSet;
-import javax.swing.text.StyleConstants;
-import javax.swing.text.StyledDocument;
 
 import model.ProcessOperation;
 import service.processor.ProcessorInterface;
 import template.ProcessDialog;
+import template.component.OperationReportComponent;
 import template.panel.ProcessorComponentInterface;
 
 /**
@@ -49,45 +42,16 @@ public class ProcessAction implements ActionListener
         ProcessDialog processDialog = new ProcessDialog(this.parent);
 
         if (processDialog.isSimulate()) {
-            List<ProcessOperation> renamedFiles = processor.simulate();
+            List<ProcessOperation> operations = processor.simulate();
 
-            if (0 == renamedFiles.size()) {
+            if (0 == operations.size()) {
                 JOptionPane.showMessageDialog(this.parent, "No file processed");
             } else {
-                JOptionPane.showMessageDialog(this.parent, this.generateReport(renamedFiles));
+                JOptionPane.showMessageDialog(this.parent, new OperationReportComponent(operations));
             }
         } else if (processDialog.isProcess()) {
             int count = processor.process();
             JOptionPane.showMessageDialog(this.parent, String.format("%d file(s) processed", count));
         }
-    }
-
-    protected JScrollPane generateReport(List<ProcessOperation> operations)
-    {
-        JTextPane textPane = new JTextPane();
-        StyledDocument doc = textPane.getStyledDocument();
-
-        SimpleAttributeSet centeredText = new SimpleAttributeSet();
-        StyleConstants.setAlignment(centeredText, StyleConstants.ALIGN_CENTER);
-
-        for (ProcessOperation operation : operations) {
-            try {
-                String line = String.format("%s\n", operation.getMessage());
-                doc.insertString(doc.getLength(), line, centeredText);
-            } catch (BadLocationException e) {
-            }
-        }
-
-        JScrollPane scrollPane = new JScrollPane(textPane);
-        int width = textPane.getPreferredSize().width + 50;
-        int height = Math.min(
-            textPane.getPreferredSize().height,
-            Toolkit.getDefaultToolkit().getScreenSize().height - 200
-        );
-        scrollPane.setPreferredSize(new Dimension(width, height));
-
-        textPane.setCaretPosition(0);
-
-        return scrollPane;
     }
 }
