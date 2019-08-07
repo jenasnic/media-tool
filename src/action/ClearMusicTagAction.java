ae313f47981eeef06a2cd07564691502d9c4c5e1
JC <jc@jc.com>
2019-08-07 16:05:41 +0200

    Use processor for music tag cleaner

--- src/action/ClearMusicTagAction.java
+++ src/action/ClearMusicTagAction.java
@@ -2,30 +2,32 @@ package action;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.SwingWorker.StateValue;
 
-import com.mpatric.mp3agic.Mp3File;
-
-import filter.MusicFileFilter;
-import service.FileFinder;
+import service.processor.MusicTagCleanProcessor;
+import template.ProgressDialog;
 import template.component.FolderSelectionComponent;
 
 /**
  * Define action to clear MP3 tag.
  */
-public class ClearMusicTagAction implements ActionListener
+public class ClearMusicTagAction implements ActionListener, PropertyChangeListener
 {
     protected JFrame parent;
     protected FolderSelectionComponent folderSelectionComponent;
-    protected int counter;
+    protected ProgressDialog progressDialog;
+    protected MusicTagCleanProcessor processor;
 
     public ClearMusicTagAction(JFrame parent, FolderSelectionComponent folderSelectionComponent)
     {
         this.parent = parent;
         this.folderSelectionComponent = folderSelectionComponent;
+        this.progressDialog = new ProgressDialog(parent);
     }
 
     @Override
@@ -39,38 +41,39 @@ public class ClearMusicTagAction implements ActionListener
 
         int confirmation = JOptionPane.showConfirmDialog(this.parent, "All tags will be removed. Confirm process ?", "Confirm", JOptionPane.YES_NO_OPTION);
         if (JOptionPane.YES_OPTION == confirmation) {
-            this.counter = 0;
-            FileFinder fileFinder = new FileFinder(new MusicFileFilter(this.folderSelectionComponent.isRecurisve()));
-            this.clearTagInFolder(this.folderSelectionComponent.getFolder(), this.folderSelectionComponent.isRecurisve(), fileFinder);
-            JOptionPane.showMessageDialog(this.parent, String.format("%d file(s) processed", this.counter));
+            this.processor = new MusicTagCleanProcessor(
+                this.folderSelectionComponent.getFolder(),
+                this.folderSelectionComponent.isRecurisve(),
+                this.parent
+            );
+            this.processor.addPropertyChangeListener(this);
+            this.processor.execute();
+
+            this.progressDialog.setVisible(true);
         }
     }
 
-    protected void clearTagInFolder(String folderToProcess, boolean recursive, FileFinder fileFinder)
+    @Override
+    public void propertyChange(PropertyChangeEvent event)
     {
-        File[] files = fileFinder.getSortedFiles(folderToProcess);
+        if (event.getNewValue() instanceof Integer) {
+            this.progressDialog.setProgress((int)event.getNewValue());
 
-        for (File file : files) {
-            if (file.isDirectory()) {
-                this.clearTagInFolder(file.getAbsolutePath(), recursive, fileFinder);
-            } else if (file.isFile()) {
+            return;
+        }
 
-                String initialFilename = file.getPath();
-                String newFilename = String.format("%s.tagged", file.getPath());
+        if (event.getNewValue() instanceof StateValue && event.getNewValue() == StateValue.DONE) {
+            this.progressDialog.dispose();
 
-                try {
-                    Mp3File mp3 = new Mp3File(file);
-                    mp3.removeId3v1Tag();
-                    mp3.removeId3v2Tag();
-                    mp3.removeCustomTag();
-                    mp3.save(newFilename);
+            try {
+                int count = this.processor.get().size();
 
-                    file.delete();
-                    File taggedFile = new File(newFilename);
-                    taggedFile.renameTo(new File(initialFilename));
-                    this.counter++;
-                } catch (Exception e) {
+                if (0 == count) {
+                    JOptionPane.showMessageDialog(this.parent, "No file processed");
+                } else {
+                    JOptionPane.showMessageDialog(this.parent, String.format("%d file(s) processed", count));
                 }
+            } catch (Exception ex) {
             }
         }
     }
